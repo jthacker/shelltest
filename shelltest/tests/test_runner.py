6b9d9c33b4d68a008bb992b9a11ab2f02a4d5cbd
jthacker <jthacker@gmail.com>
2016-02-26 10:29:20 -0600

    Update runner tests to use parameters

--- shelltest/tests/test_runner.py
+++ shelltest/tests/test_runner.py
@@ -6,21 +6,19 @@ import pytest
 from shelltest.shelltest import ShellTest, ShellTestSource, ShellTestRunner
 
 
-@pytest.fixture
-def tests():
-    return [ShellTest('echo hello', 'hello\n', ShellTestSource('', 0)),
-            ShellTest('echo $?', '0\n', ShellTestSource('', 2))]
+def runner(tests):
+    tests = [ShellTest(cmd, output, ShellTestSource('', 0)) for cmd, output in tests]
+    return ShellTestRunner(tests)
 
 
-def test_run(tests):
-    r = ShellTestRunner(tests)
-    results = r.run()
-    assert len(results) == 2
-    assert results[0].success
-    assert results[0].ret_code == 0
-    assert results[0].test == tests[0]
-    assert results[0].actual_output == tests[0].expected_output
-    assert results[1].success
-    assert results[1].ret_code == 0
-    assert results[1].test == tests[1]
-    assert results[1].actual_output == tests[1].expected_output
+@pytest.mark.parametrize("cmd,output,ret_code,success", (
+    ('echo hello', 'hello\n', 0, True),
+    ('echo $?', '0\n', 0, True),
+    ('exit 42', '', 42, True)))
+def test_echo(cmd, output, ret_code, success):
+    r = runner([(cmd, output)])
+    res = r.run()[0]
+    assert res.success == success
+    assert res.ret_code == ret_code
+    assert res.test == r.tests[0]
+    assert res.actual_output == output
